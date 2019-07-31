33a5b961b6d0c7d383350867a5c5f4cb60a0e364
Tiago Oliveira <tiago.oliveira@d0007azurecloudtotvscom.onmicrosoft.com>
2019-07-31 10:04:06 -0300

    .

--- PDV.Test/PDV.Test.UI/1. CommonMethods/ValidateSwitch.cs
+++ PDV.Test/PDV.Test.UI/1. CommonMethods/ValidateSwitch.cs
@@ -22,6 +22,11 @@ namespace PDV.Test.UI._1._CommonMethods
                 Assert.Fail(Msg);
             }
 
+            // By locator    = identificação do componente (xpath, name, id...)
+            // Nometributo   = nome do atributo que deseja pegar o valor
+            // ValorAtributo = Valor do atributo escolhido
+            // MSg           = Mensagem de retorno ao falhar o teste
+
         }
     }
 }
