ec8506beb4447e83d78352993b03cdaee6c41c2c
Tiago Oliveira <tiago.oliveira@d0007azurecloudtotvscom.onmicrosoft.com>
2019-08-21 08:15:09 -0300

    .

---
+++ PDV.Test/PDV.Test.UI/ADMIN/PageObjects/Po_Menus.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using PDV.Test.UI.POS.CommonMethods;
+using System.Threading;
+
+namespace PDV.Test.UI.ADMIN.PageObjects.Menus
+{
+    public class Po_Menus
+    {
+        private IWebDriver driver;
+        private WaitElement Wait;
+
+        public Po_Menus(IWebDriver driver)
+        {
+            this.driver = driver;
+            Wait = new WaitElement(driver);
+        }
+
+        public void ValidarTituloLista(By locator, string TituloLista)
+        {
+            Wait.LocateElement(locator);
+            var titulo = driver.FindElement(locator).Text;
+            Assert.AreEqual(TituloLista, titulo);
+        }
+
+        public void Inicio()
+        {
+            Wait.LocateElementAndClick(By.XPath("//div[1]/thf-menu-item/a/div/div"));
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+
+        public void Item()
+        {
+            Wait.LocateElementAndClick(By.XPath("//thf-menu-item/div/div[1]/span"));
+        }
+
+        public void Configuracoes()
+        {
+            Wait.LocateElementAndClick(By.XPath("//div[4]/thf-menu-item/div/div[1]/div"));
+        }
+
+        public void Produtos()
+        {
+            Item();
+
+            //Produtos
+            Wait.LocateElementAndClick(By.XPath("//div[2]/div[1]/thf-menu-item/a/div/div"));
+
+            ValidarTituloLista(By.XPath("//div[1]/button/span[2]"), "PRODUTOS");
+        }
+
+        public void Modificadores()
+        {
+            Item();
+
+            //Modificadores
+            Wait.LocateElementAndClick(By.XPath("//div[2]/div[2]/thf-menu-item/a/div/div"));
+
+            ValidarTituloLista(By.XPath("//div[1]/button/span[2]"), "MODIFICADORES");
+        }
+
+        public void GruposDeProdutos()
+        {
+            Item();
+
+            //Grupo de Produtos
+            Wait.LocateElementAndClick(By.XPath("//div/div[2]/div[3]/thf-menu-item/a"));
+
+            ValidarTituloLista(By.XPath("//div[1]/button/span[2]"), "GRUPOS");
+
+            //driver.Navigate().Refresh();
+        }
+
+        public void PDV()
+        {
+            Configuracoes();
+
+            //PDV
+            Wait.LocateElementAndClick(By.XPath("//div[4]/thf-menu-item/div/div[2]/div[1]/thf-menu-item/a/div/div"));
+
+            ValidarTituloLista(By.XPath("//div/ni-collapsible-widget/div/div[1]/button/span[2]"), "PONTOS DE VENDA");
+        }
+
+
+
+
+    }
+}
---
+++ PDV.Test/PDV.Test.UI/ADMIN/TestCase/CN 02_Item/CN 02.01_Produtos/TC_Produtos.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PDV.Test.UI.ADMIN.PageObjects;
+using PDV.Test.UI.ADMIN.PageObjects.Menus;
+using PDV.Test.UI.CommonMethods.Utils;
+using PDV.Test.UI.PageObjects.PDV;
+using System;
+
+namespace PDV.Test.UI.ADMIN.TestCase.CN_02_Item.CN_02._01_Produtos
+{
+    [TestClass]
+    public class TC_Produtos
+    {
+        private IWebDriver driver;
+        private HomePageTHExPOS Url;
+        private PoLogin_PDV Login;
+        private Po_Menus Menu;
+        private Po_Produto Pr;
+
+        public TC_Produtos()
+        {
+            driver = new ChromeDriver();
+            Url = new HomePageTHExPOS(driver);
+            Login = new PoLogin_PDV(driver);
+            Menu = new Po_Menus(driver);
+            Pr = new Po_Produto(driver);
+        }
+
+        [TestMethod]
+        public void AdicionarProduto()
+        {
+            Url.AdminTST();
+            Login.Admin();
+            Menu.Produtos();
+            Pr.BtnAdicionarProduto();
+            Pr.DadosProduto("Torta de limão - Aut", "002", "Sobremesas - Aut", "KG - Quilo", "4,50");
+            Pr.Detalhes("Chá - Aut");
+            Pr.Fiscais();
+            Pr.BtnSalvar();
+            Pr.ValidarCadastro("Torta de limão - Aut");
+            Menu.Inicio();
+        }
+    }
+}
